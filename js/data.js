17a40f3f8ac055ff869111d5a6319927b674a353
slipfly <slipfly@gmail.com>
2019-07-25 21:24:38 +0300

    исправлено для линтера

--- js/data.js
+++ js/data.js
@@ -4,19 +4,19 @@
   var AMOUNT = 4;
   var wizards = [];
   var generateWizards = function (amount) {
-      for (var i = 0; i < amount; i++) {
-        var wizard = {};
-        var randomNameNumber = window.util.getRandomFromArray(window.util.wizardsNames);
-        var randomSurnameNumber = window.util.getRandomFromArray(window.util.wizardsSurnames);
-        var randomCoatNumber = window.util.getRandomFromArray(window.util.wizardsCoats);
-        var randomEyesNumber = window.util.getRandomFromArray(window.util.wizardsEyes);
+    for (var i = 0; i < amount; i++) {
+      var wizard = {};
+      var randomNameNumber = window.util.getRandomFromArray(window.util.wizardsNames);
+      var randomSurnameNumber = window.util.getRandomFromArray(window.util.wizardsSurnames);
+      var randomCoatNumber = window.util.getRandomFromArray(window.util.wizardsCoats);
+      var randomEyesNumber = window.util.getRandomFromArray(window.util.wizardsEyes);
 
-        wizard.name = window.util.wizardsNames[randomNameNumber] + ' ' + window.util.wizardsSurnames[randomSurnameNumber];
-        wizard.coatColor = window.util.wizardsCoats[randomCoatNumber];
-        wizard.eyesColor = window.util.wizardsEyes[randomEyesNumber];
+      wizard.name = window.util.wizardsNames[randomNameNumber] + ' ' + window.util.wizardsSurnames[randomSurnameNumber];
+      wizard.coatColor = window.util.wizardsCoats[randomCoatNumber];
+      wizard.eyesColor = window.util.wizardsEyes[randomEyesNumber];
 
-        wizards.push(wizard);
-      }
+      wizards.push(wizard);
+    }
   };
 
   generateWizards(AMOUNT);
